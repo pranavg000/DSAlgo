8afed605bbcbd19b8121fa5f7c695610bef73134
Pranav <pranavgupta000@gmail.com>
2019-08-03 15:16:51 +0530

    adding prime_millerrabin

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Added Sum, Diff, Mult of 2 numbers
+# Added Sum, Diff, Mult, Div of 2 numbers
+# Also added prime.cpp and prime_millerrabin.cpp, the second one uses millerrabin method to check primality
---
+++ prime_millerrabin.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli unsigned long long int
+
+power(lli a,lli d,lli n)
+{lli pro=1;
+a=a%n;
+    while(d>0)
+    {if(d%2!=0)
+    {
+        pro=(pro*a)%n;
+        d--;
+    }
+        d=d/2;
+        a=(a*a)%n;
+
+
+    }
+    return pro;
+}
+
+bool miller(lli n,lli d)
+{
+    lli a=2+rand()%(n-3);
+
+lli x=power(a,d,n);
+    if(x==1||x==n-1) return true;
+
+    while(d!=n-1)
+    {
+        x=x*x%n;
+        d*=2;
+        if(x==1) return false;
+        if(x==n-1) return true;
+    }
+    return false;
+}
+bool prime(lli n,int p)
+{
+    if(n<=1)
+    return false;
+    if(n<=3)
+    return true;
+    if(n%2==0)
+    return false;
+
+
+    lli l=n-1;
+    while(l%2==0)
+        l=l/2;
+    for(int i=0;i<p;i++)
+        if(!miller(n,l)) return false;
+    return true;
+}
+int main()
+{
+   lli n,p=4;
+   cin>>n;
+   if(prime(n,p))
+    cout<<"Prime"<<endl;
+   else
+   cout<<"Not prime"<<endl;
+
+    return 0;
+}
