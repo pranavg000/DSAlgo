220e5565894db0d67ba041368b08d664885702fc
Pranav Gupta <pranavgupta000@gmail.com>
2019-10-21 16:32:12 +0530

    updating ec.cpp

--- PM3/multicolors/extra_credit.cpp
+++ PM3/multicolors/extra_credit.cpp
@@ -9,27 +9,29 @@ using namespace std;
 #define pb push_back
 #define N 1000
 
-int col[N][N];
-bool visited[N][N];
+
 int m,n;
 
 int size;
  
-void dfs(int x, int y, int color){
+void dfs(vector<vector<int> > &col, vector<vector<bool> > &visited, int x, int y, int color){
     if(!visited[x][y]&&col[x][y]==color){
 	    visited[x][y]=1;
 	    size++;
 	    //cout<<x<<" "<<y<<endl;
-	    if(x+1<m) dfs(x+1,y,color);
-	    if(x-1>=0) dfs(x-1,y,color);
-	    if(y+1<n) dfs(x,y+1,color);
-	    if(y-1>=0) dfs(x,y-1,color);
+	    if(x+1<m) dfs(col, visited, x+1,y,color);
+	    if(x-1>=0) dfs(col, visited, x-1,y,color);
+	    if(y+1<n) dfs(col, visited, x,y+1,color);
+	    if(y-1>=0) dfs(col, visited, x,y-1,color);
 		}
 }
 
 int main(){
 
 cin>>m>>n;
+vector<vector<int> > col( m , vector<int> (n,0));
+vector<vector<bool> > visited( m , vector<bool> (n,false));
+
 int big=-1;
 int k;
 cin>>k;
@@ -48,7 +50,7 @@ loop(i,0,m){
 	loop(j,0,n){
 		if(!visited[i][j]){
 			size=0;
-			dfs(i,j, col[i][j]);
+			dfs(col, visited, i,j, col[i][j]);
 			big = max(big, size);
 		}
 	}
